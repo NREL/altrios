b94ddcd2390bb664d7ed3050e6bf244905c23d77
Chad Baker <chad.baker@nrel.gov>
2023-10-18 15:42:18 -0600

    added echo prints for ease of use

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,7 +1,11 @@
 # assumes a python environment has been created and activated
+echo "Testing rust"
 (cd rust/ && cargo test --workspace) && \
 # pip install -qe ".[dev]" && \ 
 # assumes `pip install -qe ".[dev]"` has been run already
+echo "Building python API"
 maturin develop --release && \
+echo "Running python tests"
 pytest -v tests && \
+echo "Verifying that demos run"
 (cd applications/demos/ && python sim_manager_demo.py && python rollout_demo.py)

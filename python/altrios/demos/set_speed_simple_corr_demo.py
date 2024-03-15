05aee876b7903197f29b7f1b223b5dc51f88d9ea
Chad Baker <chad.baker@nrel.gov>
2024-03-15 14:40:46 -0600

    moved plotting to inside if statement

--- python/altrios/demos/set_speed_simple_corr_demo.py
+++ python/altrios/demos/set_speed_simple_corr_demo.py
@@ -21,7 +21,7 @@ train_config = alt.TrainConfig(
     cars_empty=50,
     cars_loaded=50,
     rail_vehicle_type="Manifest",
-    train_type=alt.TrainType.Freight,
+    train_type=None,
     train_length_meters=None,
     train_mass_kilograms=None,
 )
@@ -78,61 +78,61 @@ print(f'Time to simulate: {t1 - t0:.5g}')
 # pull out solved locomotive for plotting convenience
 loco0:alt.Locomotive = train_sim.loco_con.loco_vec.tolist()[0]
 
-fig, ax = plt.subplots(4, 1, sharex=True)
-ax[0].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.pwr_whl_out_watts,
-    label="tract pwr",
-)
-ax[0].set_ylabel('Power')
-ax[0].legend()
-
-ax[1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.res_aero_newtons,
-    label='aero',
-)
-ax[1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.res_rolling_newtons,
-    label='rolling',
-)
-ax[1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.res_curve_newtons,
-    label='curve',
-)
-ax[1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.res_bearing_newtons,
-    label='bearing',
-)
-ax[1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.history.res_grade_newtons,
-    label='grade',
-)
-ax[1].set_ylabel('Force [N]')
-ax[1].legend()
-
-ax[-1].plot(
-    np.array(train_sim.history.time_seconds) / 3_600,
-    train_sim.speed_trace.speed_meters_per_second,
-)
-ax[-1].set_xlabel('Time [hr]')
-ax[-1].set_ylabel('Speed [m/s]')
-
-ax[2].plot(
-    np.array(train_sim.history.time_seconds) / 3_600, 
-    np.array(loco0.res.history.soc)
-)
-
-ax[2].set_ylabel('SOC')
-ax[2].legend()
-
-plt.suptitle("Set Speed Train Sim Demo")
-
 if SHOW_PLOTS:
+    fig, ax = plt.subplots(4, 1, sharex=True)
+    ax[0].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.pwr_whl_out_watts,
+        label="tract pwr",
+    )
+    ax[0].set_ylabel('Power')
+    ax[0].legend()
+
+    ax[1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.res_aero_newtons,
+        label='aero',
+    )
+    ax[1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.res_rolling_newtons,
+        label='rolling',
+    )
+    ax[1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.res_curve_newtons,
+        label='curve',
+    )
+    ax[1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.res_bearing_newtons,
+        label='bearing',
+    )
+    ax[1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.history.res_grade_newtons,
+        label='grade',
+    )
+    ax[1].set_ylabel('Force [N]')
+    ax[1].legend()
+
+    ax[-1].plot(
+        np.array(train_sim.history.time_seconds) / 3_600,
+        train_sim.speed_trace.speed_meters_per_second,
+    )
+    ax[-1].set_xlabel('Time [hr]')
+    ax[-1].set_ylabel('Speed [m/s]')
+
+    ax[2].plot(
+        np.array(train_sim.history.time_seconds) / 3_600, 
+        np.array(loco0.res.history.soc)
+    )
+
+    ax[2].set_ylabel('SOC')
+    ax[2].legend()
+
+    plt.suptitle("Set Speed Train Sim Demo")
+
     plt.tight_layout()
     plt.show()
 

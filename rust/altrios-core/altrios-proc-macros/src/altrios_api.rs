178150fbe541c513a5aae5114a1e214d86d9b021
Chad Baker <chad.baker@nrel.gov>
2024-04-02 11:49:10 -0600

    removed overly restrictive check from `Pyo3Vec stuff in proc macro

--- rust/altrios-core/altrios-proc-macros/src/altrios_api.rs
+++ rust/altrios-core/altrios-proc-macros/src/altrios_api.rs
@@ -78,79 +78,74 @@ pub(crate) fn altrios_api(attr: TokenStream, item: TokenStream) -> TokenStream {
     } else if let syn::Fields::Unnamed(syn::FieldsUnnamed { unnamed, .. }) = &mut ast.fields {
         // tuple struct
         let ident_str = ast.ident.to_string();
-        if ["Vec", "Network", "Path"]
-            .iter()
-            .any(|&x| ident_str.contains(x))
-        {
-            assert!(unnamed.len() == 1);
-            for field in unnamed.iter() {
-                let ftype = field.ty.clone();
-                if let syn::Type::Path(type_path) = ftype.clone() {
-                    let type_str = type_path.clone().into_token_stream().to_string();
-                    if type_str.contains("Vec") {
-                        let re = Regex::new(r"Vec < (.+) >").unwrap();
-                        // println!("{}", type_str);
-                        // println!("{}", &re.captures(&type_str).unwrap()[1]);
-                        let contained_dtype: TokenStream2 = re.captures(&type_str).unwrap()[1]
-                            .to_string()
-                            .parse()
-                            .unwrap();
-                        py_impl_block.extend::<TokenStream2>(
-                            quote! {
-                                #[new]
-                                /// Rust-defined `__new__` magic method for Python used exposed via PyO3.
-                                fn __new__(v: Vec<#contained_dtype>) -> Self {
-                                    Self(v)
-                                }
-                                /// Rust-defined `__repr__` magic method for Python used exposed via PyO3.
-                                fn __repr__(&self) -> String {
-                                    format!("Pyo3Vec({:?})", self.0)
-                                }
-                                /// Rust-defined `__str__` magic method for Python used exposed via PyO3.
-                                fn __str__(&self) -> String {
-                                    format!("{:?}", self.0)
-                                }
-                                /// Rust-defined `__getitem__` magic method for Python used exposed via PyO3.
-                                /// Prevents the Python user getting item directly using indexing.
-                                fn __getitem__(&self, _idx: usize) -> anyhow::Result<()> {
-                                    bail!(PyNotImplementedError::new_err(
-                                        "Getting Rust vector value at index is not implemented.
-                                        Run `tolist` method to convert to standalone Python list.",
-                                    ))
-                                }
-                                /// Rust-defined `__setitem__` magic method for Python used exposed via PyO3.
-                                /// Prevents the Python user setting item using indexing.
-                                fn __setitem__(&mut self, _idx: usize, _new_value: #contained_dtype) -> anyhow::Result<()> {
-                                    bail!(PyNotImplementedError::new_err(
-                                        "Setting list value at index is not implemented.
-                                        Run `tolist` method, modify value at index, and
-                                        then set entire list.",
-                                    ))
-                                }
-                                /// PyO3-exposed method to convert vec-containing struct to Python list. 
-                                fn tolist(&self) -> anyhow::Result<Vec<#contained_dtype>> {
-                                    Ok(self.0.clone())
-                                }
-                                /// Rust-defined `__len__` magic method for Python used exposed via PyO3.
-                                /// Returns the length of the Rust vector.
-                                fn __len__(&self) -> usize {
-                                    self.0.len()
-                                }
-                                /// PyO3-exposed method to check if the vec-containing struct is empty.
-                                fn is_empty(&self) -> bool {
-                                    self.0.is_empty()
-                                }
+        assert!(unnamed.len() == 1);
+        for field in unnamed.iter() {
+            let ftype = field.ty.clone();
+            if let syn::Type::Path(type_path) = ftype.clone() {
+                let type_str = type_path.clone().into_token_stream().to_string();
+                if type_str.contains("Vec") {
+                    let re = Regex::new(r"Vec < (.+) >").unwrap();
+                    // println!("{}", type_str);
+                    // println!("{}", &re.captures(&type_str).unwrap()[1]);
+                    let contained_dtype: TokenStream2 = re.captures(&type_str).unwrap()[1]
+                        .to_string()
+                        .parse()
+                        .unwrap();
+                    py_impl_block.extend::<TokenStream2>(
+                        quote! {
+                            #[new]
+                            /// Rust-defined `__new__` magic method for Python used exposed via PyO3.
+                            fn __new__(v: Vec<#contained_dtype>) -> Self {
+                                Self(v)
                             }
-                        );
-                        impl_block.extend::<TokenStream2>(quote! {
-                            impl #ident{
-                                /// Implement the non-Python `new` method.
-                                pub fn new(value: Vec<#contained_dtype>) -> Self {
-                                    Self(value)
-                                }
+                            /// Rust-defined `__repr__` magic method for Python used exposed via PyO3.
+                            fn __repr__(&self) -> String {
+                                format!("Pyo3Vec({:?})", self.0)
                             }
-                        });
-                    }
+                            /// Rust-defined `__str__` magic method for Python used exposed via PyO3.
+                            fn __str__(&self) -> String {
+                                format!("{:?}", self.0)
+                            }
+                            /// Rust-defined `__getitem__` magic method for Python used exposed via PyO3.
+                            /// Prevents the Python user getting item directly using indexing.
+                            fn __getitem__(&self, _idx: usize) -> anyhow::Result<()> {
+                                bail!(PyNotImplementedError::new_err(
+                                    "Getting Rust vector value at index is not implemented.
+                                    Run `tolist` method to convert to standalone Python list.",
+                                ))
+                            }
+                            /// Rust-defined `__setitem__` magic method for Python used exposed via PyO3.
+                            /// Prevents the Python user setting item using indexing.
+                            fn __setitem__(&mut self, _idx: usize, _new_value: #contained_dtype) -> anyhow::Result<()> {
+                                bail!(PyNotImplementedError::new_err(
+                                    "Setting list value at index is not implemented.
+                                    Run `tolist` method, modify value at index, and
+                                    then set entire list.",
+                                ))
+                            }
+                            /// PyO3-exposed method to convert vec-containing struct to Python list. 
+                            fn tolist(&self) -> anyhow::Result<Vec<#contained_dtype>> {
+                                Ok(self.0.clone())
+                            }
+                            /// Rust-defined `__len__` magic method for Python used exposed via PyO3.
+                            /// Returns the length of the Rust vector.
+                            fn __len__(&self) -> usize {
+                                self.0.len()
+                            }
+                            /// PyO3-exposed method to check if the vec-containing struct is empty.
+                            fn is_empty(&self) -> bool {
+                                self.0.is_empty()
+                            }
+                        }
+                    );
+                    impl_block.extend::<TokenStream2>(quote! {
+                        impl #ident{
+                            /// Implement the non-Python `new` method.
+                            pub fn new(value: Vec<#contained_dtype>) -> Self {
+                                Self(value)
+                            }
+                        }
+                    });
                 }
             }
         }

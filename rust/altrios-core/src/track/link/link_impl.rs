5d1fd059fa0d295289e76fc7f92651778cec316d
Chad Baker <chad.baker@nrel.gov>
2024-06-18 14:36:09 -0600

    incremental progress toward better error message

--- rust/altrios-core/src/track/link/link_impl.rs
+++ rust/altrios-core/src/track/link/link_impl.rs
@@ -323,7 +323,11 @@ impl SerdeAPI for Network {
         })?;
         let mut network = match Self::from_reader(file, extension) {
             Ok(network) => network,
-            Err(err) => NetworkOld::from_file(filepath).with_context(|| err)?.into(),
+            Err(err) => NetworkOld::from_file(filepath)
+                .map_err(|old_err| {
+                    anyhow!("attempting to load as `Network`:\n{}\nattempting to load as `NetworkOld`:\n{}", err, old_err)
+                })?
+                .into(),
         };
         network.init()?;
 

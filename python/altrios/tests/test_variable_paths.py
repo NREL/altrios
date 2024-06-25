9d61d651906b68263b3a20b0cf79af931a577594
Robin Steuteville <robin.steuteville@nrel.gov>
2024-06-25 17:11:52 -0400

    fixing history function

--- python/altrios/tests/test_variable_paths.py
+++ python/altrios/tests/test_variable_paths.py
@@ -6,15 +6,15 @@ class TestParamPath(unittest.TestCase):
         gen = alt.Generator.default()
 
         with open(alt.resources_root() / "benchmark_variable_paths/generator_variable_paths.txt") as file:
-            baseline_variable_paths = file.readlines()
+            baseline_variable_paths = [line.strip() for line in file.readlines()]
         with open(alt.resources_root() / "benchmark_variable_paths/generator_history_paths.txt") as file:
-            baseline_history_variable_paths = file.readlines()
+            baseline_history_variable_paths = [line.strip() for line in file.readlines()]
 
         # compare relative variable paths within Generator to baseline
-        assert(baseline_variable_paths.sort()==gen.variable_path_list().sort())
+        assert(sorted(baseline_variable_paths)==sorted(gen.variable_path_list()))
 
         # compare relative history variable paths within Generator to baseline
-        assert(baseline_history_variable_paths.sort()==gen.history_path_list().sort())
+        assert(sorted(baseline_history_variable_paths)==sorted(gen.history_path_list()))
 
 
 if __name__ == "__main__":

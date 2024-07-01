45191af499a783e58a375a63e612a40cb3864f8f
Bruchon, Matthew <Matthew.Bruchon@nrel.gov>
2024-07-01 18:27:08 -0400

    Enable charging headroom for all EVs

--- python/altrios/train_planner.py
+++ python/altrios/train_planner.py
@@ -1021,18 +1021,16 @@ def run_train_planner(
                         .rank().alias('rank').cast(pl.UInt32)
                         ).with_row_count().sort('row_nr'))
                     dispatched = dispatched.sort('Locomotive_ID')
-                    loco_types = dispatched.get_column('Locomotive_Type')
                     loco_start_soc_pct = dispatched.select(pl.col('SOC_J') / pl.col('Capacity_J')).to_series()
                     locos = [
                         config.loco_info[config.loco_info['Locomotive_Type']==loco_type]['Rust_Loco'].to_list()[0].clone() 
-                        for loco_type in loco_types
+                        for loco_type in dispatched.get_column('Locomotive_Type')
                     ]
-                    
                     [alt.set_param_from_path(
                         locos[i], 
                         "res.state.soc", 
                         loco_start_soc_pct[i]
-                    ) for i in range(len(locos)) if loco_types[i] == 'BEL']
+                    ) for i in range(len(locos)) if dispatched.get_column('Fuel_Type')[i] == 'Electricity']
 
                     loco_con = alt.Consist(
                         loco_vec=locos,
@@ -1042,7 +1040,6 @@ def run_train_planner(
                     init_train_state = alt.InitTrainState(
                         time_seconds=current_time * 3600
                     )
-                    
                     tsb = alt.TrainSimBuilder(
                         train_id=train_id,
                         origin_id=this_train['Origin'],

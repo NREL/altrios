5da2fa0f850e7906c81b67eea837fc09dc1fc304
Chad Baker <chad.baker@nrel.gov>
2024-07-19 14:59:43 -0600

    beefed up error message

--- rust/altrios-core/altrios-proc-macros/src/altrios_api/altrios_api_utils.rs
+++ rust/altrios-core/altrios-proc-macros/src/altrios_api/altrios_api_utils.rs
@@ -271,7 +271,10 @@ pub(crate) fn impl_getters_and_setters(
                 uom::si::velocity::meter_per_second,
                 uom::si::velocity::mile_per_hour
             ),
-            _ => abort!(inner_path.span(), "Unknown si quantity!"),
+            _ => abort!(
+                inner_path.span(),
+                "Unknown si quantity! Make sure it's implemented in `impl_getters_and_setters`"
+            ),
         };
         for (field_units, unit_name) in &unit_impls {
             impl_get_set_si(

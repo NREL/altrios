fa10555bcbe4ad6a7d726079ca778a1ec7afb27d
Chad Baker <chad.baker@nrel.gov>
2025-02-11 11:19:36 -0700

    bespoke dictionary flattening works great and all tests pass

--- python/altrios/__init__.py
+++ python/altrios/__init__.py
@@ -1,12 +1,10 @@
 from importlib.metadata import version
 __version__ = version("altrios")
 
-from pathlib import Path
-import re
 import numpy as np
+import re
 import logging
-import inspect
-from typing import List, Union, Dict, Optional
+from typing import List, Union, Dict, Optional, Any
 from typing_extensions import Self
 import pandas as pd
 import polars as pl
@@ -14,11 +12,11 @@ import polars as pl
 from altrios.loaders.powertrain_components import _res_from_excel
 from altrios.utilities import set_param_from_path  # noqa: F401
 from altrios.utilities import copy_demo_files  # noqa: F401
-from altrios import utilities as utils
-from altrios.utilities import package_root, resources_root
+from altrios import utilities as utils  # noqa: F401
+from altrios.utilities import package_root, resources_root  # noqa: F401
 # make everything in altrios_pyo3 available here
-from altrios.altrios_pyo3 import *
-from altrios import *
+from altrios.altrios_pyo3 import *  # noqa: F403
+from altrios import *  # noqa: F403
 
 DEFAULT_LOGGING_CONFIG = dict(
     format="%(asctime)s.%(msecs)03d | %(filename)s:%(lineno)s | %(levelname)s: %(message)s",
@@ -29,13 +27,15 @@ DEFAULT_LOGGING_CONFIG = dict(
 logging.basicConfig(**DEFAULT_LOGGING_CONFIG)
 logger = logging.getLogger(__name__)
 
+
 def __array__(self):
     return np.array(self.tolist())
 
+
 # creates a list of all python classes from rust structs that need python-side serde helpers
 ACCEPTED_RUST_STRUCTS = [
-    attr for attr in altrios_pyo3.__dir__() if not attr.startswith("__") and isinstance(getattr(altrios_pyo3, attr), type) and
-    attr[0].isupper() 
+    attr for attr in altrios_pyo3.__dir__() if not attr.startswith("__") and isinstance(getattr(altrios_pyo3, attr), type) and  # noqa: F405
+    attr[0].isupper()
 ]
 
 # TODO connect to crate features
@@ -45,6 +45,7 @@ data_formats = [
     # 'toml',
     'json',
 ]
+
 
 def to_pydict(self, data_fmt: str = "msg_pack", flatten: bool = False) -> Dict:
     """
@@ -57,10 +58,10 @@ def to_pydict(self, data_fmt: str = "msg_pack", flatten: bool = False) -> Dict:
     assert data_fmt in data_formats, f"`data_fmt` must be one of {data_formats}"
     match data_fmt:
         case "msg_pack":
-            import msgpack
+            import msgpack  # type: ignore[import-untyped]
             pydict = msgpack.loads(self.to_msg_pack())
         case "yaml":
-            from yaml import load
+            from yaml import load  # type: ignore[import-untyped]
             try:
                 from yaml import CLoader as Loader
             except ImportError:
@@ -73,7 +74,11 @@ def to_pydict(self, data_fmt: str = "msg_pack", flatten: bool = False) -> Dict:
     if not flatten:
         return pydict
     else:
-        return next(iter(pd.json_normalize(pydict, sep=".").to_dict(orient='records')))
+        hist_len = get_hist_len(pydict)
+        assert hist_len is not None, "Cannot be flattened"
+        flat_dict = get_flattened(pydict, hist_len)
+        return flat_dict
+
 
 @classmethod
 def from_pydict(cls, pydict: Dict, data_fmt: str = "msg_pack", skip_init: bool = False) -> Self:
@@ -101,66 +106,115 @@ def from_pydict(cls, pydict: Dict, data_fmt: str = "msg_pack", skip_init: bool =
 
     return obj
 
+
+def get_flattened(obj: Dict | List, hist_len: int, prepend_str: str = "") -> Dict:
+    """
+    Flattens and returns dictionary, separating keys and indices with a `"."`
+    # Arguments
+    # - `obj`: object to flatten
+    # -  hist_len: length of any lists storing history data
+    # - `prepend_str`: prepend this to all keys in the returned `flat` dict
+    """
+    flat: Dict = {}
+    if isinstance(obj, dict):
+        for (k, v) in obj.items():
+            new_key = k if (prepend_str == "") else prepend_str + "." + k
+            if isinstance(v, dict) or (isinstance(v, list) and len(v) != hist_len):
+                flat.update(get_flattened(v, hist_len, prepend_str=new_key))
+            else:
+                flat[new_key] = v
+    elif isinstance(obj, list):
+        for (i, v) in enumerate(obj):
+            new_key = i if (prepend_str == "") else prepend_str + "." + str(i)
+            if isinstance(v, dict) or (isinstance(v, list) and len(v) != hist_len):
+                flat.update(get_flattened(v, hist_len, prepend_str=new_key))
+            else:
+                flat[new_key] = v
+    else:
+        raise TypeError("`obj` should be `dict` or `list`")
+
+    return flat
+
+
+def get_hist_len(obj: Dict) -> Optional[int]:
+    """
+    Finds nested `history` and gets lenth of first element
+    """
+    if 'history' in obj.keys():
+        return len(next(iter(obj['history'].values())))
+
+    elif next(iter(k for k in obj.keys() if re.search("(history\.\w+)$", k) is not None), None) is not None:
+        return len(next((v for (k, v) in obj.items() if re.search("(history\.\w+)$", k) is not None)))
+
+    for (k, v) in obj.items():
+        if isinstance(v, dict):
+            hist_len = get_hist_len(v)
+            if hist_len is not None:
+                return hist_len
+    return None
+
+
 def to_dataframe(self, pandas: bool = False, allow_partial: bool = False) -> Union[pd.DataFrame, pl.DataFrame]:
     """
     Returns time series results from fastsim object as a Polars or Pandas dataframe.
 
     # Arguments
     - `pandas`: returns pandas dataframe if True; otherwise, returns polars dataframe by default
-    - `allow_partial`: returns dataframe of length equal to solved time steps if simulation fails early
+    - `allow_partial`: tries to return dataframe of length equal to solved time steps if simulation fails early
     """
     obj_dict = self.to_pydict(flatten=True)
-    history_dict = {}
-    history_keys = [
-        'history',
-        'speed_trace',
-        'power_trace'
-    ]
+    history_keys = ['history.', 'speed_trace.', 'power_trace.']
+    hist_len = get_hist_len(obj_dict)
+    assert hist_len is not None
+
+    history_dict: Dict[str, Any] = {}
     for k, v in obj_dict.items():
-        if any([hk in k for hk in history_keys]):
-            history_dict[k] = v
-    try:
-        hd_len = len(next(iter(history_dict.values())))
-    except StopIteration as err:
-        raise Exception(f"{err}\n`history_dict` should not be empty by this point")
-  
-    for k, v in obj_dict.items():
-        if ("__len__" in dir(v)) and (len(v) == hd_len):
-            history_dict[k] = v
+        hk_in_k = any(hk in k for hk in history_keys)
+        if hk_in_k and ("__len__" in dir(v)):
+            if (len(v) == hist_len) or allow_partial:
+                history_dict[k] = v
 
     if allow_partial:
         cutoff = min([len(val) for val in history_dict.values()])
 
         if not pandas:
-            df = pl.DataFrame({col: val[:cutoff]
-                              for col, val in history_dict.items()})
+            try:
+                df = pl.DataFrame({col: val[:cutoff]
+                                   for col, val in history_dict.items()})
+            except Exception as err:
+                raise Exception(f"{err}\n`save_interval` may not be uniform")
         else:
-            df = pd.DataFrame({col: val[:cutoff]
-                              for col, val in history_dict.items()})
+            try:
+                df = pd.DataFrame({col: val[:cutoff]
+                                   for col, val in history_dict.items()})
+            except Exception as err:
+                raise Exception(f"{err}\n`save_interval` may not be uniform")
+
     else:
         if not pandas:
             try:
                 df = pl.DataFrame(history_dict)
             except Exception as err:
-                raise (
-                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe`")
+                raise Exception(
+                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe` or checking for consistent save intervals")
         else:
             try:
                 df = pd.DataFrame(history_dict)
             except Exception as err:
-                raise (
-                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe`")
+                raise Exception(
+                    f"{err}\nTry passing `allow_partial=True` to `to_dataframe` or checking for consistent save intervals")
     return df
+
 
 # adds variable_path_list() and history_path_list() as methods to all classes in
 # ACCEPTED_RUST_STRUCTS
 for item in ACCEPTED_RUST_STRUCTS:
-    setattr(getattr(altrios_pyo3, item), "to_pydict", to_pydict)
-    setattr(getattr(altrios_pyo3, item), "from_pydict", from_pydict)
-    setattr(getattr(altrios_pyo3, item), "to_dataframe", to_dataframe)
+    setattr(getattr(altrios_pyo3, item), "to_pydict", to_pydict)  # noqa: F405
+    setattr(getattr(altrios_pyo3, item), "from_pydict", from_pydict)  # noqa: F405
+    setattr(getattr(altrios_pyo3, item), "to_dataframe", to_dataframe)  # noqa: F405
 
 setattr(ReversibleEnergyStorage, "from_excel", classmethod(_res_from_excel))  # noqa: F405
 setattr(Pyo3VecWrapper, "__array__", __array__)  # noqa: F405
-setattr(Pyo3Vec2Wrapper, "__array__", __array__)
-setattr(Pyo3Vec3Wrapper, "__array__", __array__)
-setattr(Pyo3VecBoolWrapper, "__array__", __array__)
+setattr(Pyo3Vec2Wrapper, "__array__", __array__)  # noqa: F405
+setattr(Pyo3Vec3Wrapper, "__array__", __array__)  # noqa: F405
+setattr(Pyo3VecBoolWrapper, "__array__", __array__)  # noqa: F405

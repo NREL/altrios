b96c1fed07067fec20d86995d7d3d68109199286
FDsteven <fdstevensdw@gmail.com>
2025-03-17 14:51:01 -0600

    Main framework done, looking for the fc state parameter to call for

--- python/altrios/demos/speed_limit_simple_corr_demo.py
+++ python/altrios/demos/speed_limit_simple_corr_demo.py
@@ -10,7 +10,7 @@ import pandas as pd
 import seaborn as sns
 
 import altrios as alt 
-def extract_bel_from_train_sim(ts: alt.TrainSim) -> list:
+def extract_bel_from_train_sim(ts: alt.SetSpeedTrainSim) -> list:
     ts_list = ts.loco_con.loco_vec.tolist()
     loco_list = []
     for loco in ts_list:
@@ -21,7 +21,7 @@ def extract_bel_from_train_sim(ts: alt.TrainSim) -> list:
         return False
     return loco_list
 
-def extract_conv_from_train_sim(ts: alt.TrainSim) -> list:
+def extract_conv_from_train_sim(ts: alt.SetSpeedTrainSim) -> list:
     ts_list = ts.loco_con.loco_vec.tolist()
     loco_list = []
     for loco in ts_list:
@@ -32,7 +32,7 @@ def extract_conv_from_train_sim(ts: alt.TrainSim) -> list:
         return False
     return loco_list
 
-def extract_hel_from_train_sim(ts: alt.TrainSim) -> list:
+def extract_hel_from_train_sim(ts: alt.SetSpeedTrainSim) -> list:
     ts_list = ts.loco_con.loco_vec.tolist()
     loco_list = []
     for loco in ts_list:
@@ -43,7 +43,7 @@ def extract_hel_from_train_sim(ts: alt.TrainSim) -> list:
         return False
     return loco_list
 
-def plot_locos_from_ts(ts:alt.TrainSim,x:str, y:str):
+def plot_locos_from_ts(ts:alt.SetSpeedTrainSim,x:str, y:str):
     """
     Extracts first instance of each loco_type and plots representative plots
     Offers several plotting options to put on x and y axis
@@ -53,6 +53,12 @@ def plot_locos_from_ts(ts:alt.TrainSim,x:str, y:str):
     first_bel = []
     first_conv = []
     first_hel = []
+    if extract_bel_from_train_sim(ts) == False:
+        print("No BEL in consist")
+    if extract_conv_from_train_sim(ts) == False:
+        print("No conventional loco in consist")
+    if extract_hel_from_train_sim(ts) == False:
+        print("No HEL in consist")
     if extract_bel_from_train_sim(ts) != False:
         first_bel = extract_bel_from_train_sim(ts)[0]
         fig, ax = plt.subplots(4, 1, sharex=True)
@@ -155,10 +161,10 @@ def plot_locos_from_ts(ts:alt.TrainSim,x:str, y:str):
 
         ax[2].plot(
             np.array(ts.history.time_seconds) / 3_600, 
-            np.array(first_conv.fc.history.soc)
+            np.array(first_conv.fc.state.pwr_out_frac_interp*pwr_out_max_watts/1e6)
         )
 
-        ax[2].set_ylabel('SOC')
+        ax[2].set_ylabel('Output MWH')
         ax[2].legend()
 
         ax[-1].plot(
@@ -177,8 +183,77 @@ def plot_locos_from_ts(ts:alt.TrainSim,x:str, y:str):
         plt.suptitle("Speed Limit Train Sim Demo")
     if extract_hel_from_train_sim(ts) != False:
         first_hel = extract_hel_from_train_sim(ts)[0]
+        fig, ax = plt.subplots(4, 1, sharex=True)
+        ax[0].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.pwr_whl_out_watts) / 1e6,
+            label="tract pwr",
+        )
+        ax[0].set_ylabel('Power [MW]')
+        ax[0].legend()
+
+        ax[1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.res_aero_newtons) / 1e3,
+            label='aero',
+        )
+        ax[1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.res_rolling_newtons) / 1e3,
+            label='rolling',
+        )
+        ax[1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.res_curve_newtons) / 1e3,
+            label='curve',
+        )
+        ax[1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.res_bearing_newtons) / 1e3,
+            label='bearing',
+        )
+        ax[1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            np.array(ts.history.res_grade_newtons) / 1e3,
+            label='grade',
+        )
+        ax[1].set_ylabel('Force [MN]')
+        ax[1].legend()
+
+        ax[2].plot(
+            np.array(ts.history.time_seconds) / 3_600, 
+            np.array(first_hel.fc.history.pwr_out_frac_interp*pwr_out_max_watts/1e6)
+        )
+        ax[2].plot(
+            np.array(ts.history.time_seconds) / 3_600, 
+            np.array(first_hel.res.history.soc)
+        )
+
+        ax[2].set_ylabel('Output MWH')
+        ax[2].legend()
+
+        ax[3].plot(
+            np.array(ts.history.time_seconds) / 3_600, 
+            np.array(first_hel.res.history.soc)
+        )
+
+        ax[3].set_ylabel('SOC')
+        ax[3].legend()
+        ax[-1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            ts.history.speed_meters_per_second,
+            label='achieved'
+        )
+        ax[-1].plot(
+            np.array(ts.history.time_seconds) / 3_600,
+            ts.history.speed_limit_meters_per_second,
+            label='limit'
+        )
+        ax[-1].set_xlabel('Time [hr]')
+        ax[-1].set_ylabel('Speed [m/s]')
+        ax[-1].legend()
+        plt.suptitle("Speed Limit Train Sim Demo")
     
-    loco0:alt.Locomotive = train_sim.loco_con.loco_vec.tolist()[0]
     plt.tight_layout()
     plt.show()
     return 
@@ -331,3 +406,4 @@ if SHOW_PLOTS:
     plt.tight_layout()
     plt.show()
 
+plot_locos_from_ts(train_sim,0,0)

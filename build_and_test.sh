a328f197ae2123283c52ace34fee8f57f48e97b8
Chad Baker <chad.baker@nrel.gov>
2025-03-19 15:45:10 -0600

    fixed dithering behavior during braking by reverting default brake parameters to the hardcoded values from 8b7d396b3e8b3457bc4eb40fbbb504b1e7c5938b

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,14 +1,10 @@
-{
-  pixi run build_and_test && echo "Complete success with pixi!"
-} || {
-  # assumes a python environment has been created and activated
-  echo "Testing rust" && \
-  cd rust/ && cargo test --workspace && cd .. && \
-  echo "Building python API" && \
-  pip install -qe ".[dev]" && \ 
-  echo "Running python tests" && \
-  pytest -v python/altrios/tests && \
-  echo "Verifying that demos run" && \
-  pytest -v python/altrios/demos && \
-  echo "Complete success!"
-}
+# assumes a python environment has been created and activated
+echo "Testing rust" && \
+cd rust/ && cargo test --workspace && cd .. && \
+echo "Building python API" && \
+pip install -qe ".[dev]" && \ 
+echo "Running python tests" && \
+pytest -v python/altrios/tests && \
+echo "Verifying that demos run" && \
+pytest -v python/altrios/demos && \
+echo "Complete success!"

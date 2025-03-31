4f11253ba0f334832963f10f461e16c47c96c9ef
FDsteven <fdstevensdw@gmail.com>
2025-03-31 08:39:52 -0600

    Adding hybridloco in set speed for debugging purpose

--- python/altrios/demos/set_speed_simple_corr_demo.py
+++ python/altrios/demos/set_speed_simple_corr_demo.py
@@ -27,9 +27,9 @@ train_config = alt.TrainConfig(
 )
 
 bel: alt.Locomotive = alt.Locomotive.default_battery_electric_loco()
-
+hel: alt.Locomotive = alt.Locomotive.default_hybrid_electric_loco()
 # construct a vector of one BEL and several conventional locomotives
-loco_vec = [bel] + [alt.Locomotive.default()] * 7
+loco_vec = [bel] + [alt.Locomotive.default()] * 7 + [hel.clone()] 
 # instantiate consist
 loco_con = alt.Consist(
     loco_vec,

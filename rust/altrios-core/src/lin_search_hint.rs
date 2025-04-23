c6346ce4e5987756bf1bc484af17922c48b605e2
Chad Baker <chad.baker@nrel.gov>
2025-04-23 11:21:02 -0600

    explicitly implemented `LinSearchHint` for `PathResCoeff` and `LinkPoint`

--- rust/altrios-core/src/lin_search_hint.rs
+++ rust/altrios-core/src/lin_search_hint.rs
@@ -22,7 +22,39 @@ pub trait LinSearchHint {
     fn calc_idx(&self, offset: si::Length, idx: usize, dir: &Dir) -> anyhow::Result<usize>;
 }
 
-impl<T: GetOffset + core::fmt::Debug> LinSearchHint for &[T] {
+impl LinSearchHint for &[LinkPoint] {
+    /// # Arguments
+    /// - `offset`: linear displacement of front of train from initial starting
+    ///   position of back of train along entire PathTPC
+    /// - `idx`: index of front (TODO: clarify this) of train within corresponding [PathResCoeff]
+    /// - `dir`: direction of train along PathTPC
+    fn calc_idx(&self, offset: si::Length, mut idx: usize, dir: &Dir) -> anyhow::Result<usize> {
+        if dir != &Dir::Bwd {
+            ensure!(
+                offset <= self.last().unwrap().get_offset(),
+                "{}\nOffset in forward direction larger than last slice offset at idx: {}!",
+                format_dbg!(),
+                idx
+            );
+            while self[idx + 1].get_offset() < offset {
+                idx += 1;
+            }
+        } else if dir != &Dir::Fwd {
+            ensure!(
+                self.first().unwrap().get_offset() <= offset,
+                "{}\nOffset in reverse direction smaller than first slice offset at idx: {}!",
+                format_dbg!(),
+                idx
+            );
+            while offset < self[idx].get_offset() {
+                idx -= 1;
+            }
+        }
+        Ok(idx)
+    }
+}
+
+impl LinSearchHint for &[PathResCoeff] {
     /// # Arguments
     /// - `offset`: linear displacement of front of train from initial starting
     ///   position of back of train along entire PathTPC

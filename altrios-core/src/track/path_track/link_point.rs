e753323943a8f71e889052bd4d7b9ae2e1d946ff
Chad Baker <chad.baker@nrel.gov>
2025-04-24 10:26:38 -0600

    removed unused trait impl

--- altrios-core/src/track/path_track/link_point.rs
+++ altrios-core/src/track/path_track/link_point.rs
@@ -69,38 +69,6 @@ impl GetOffset for LinkPoint {
     }
 }
 
-impl LinSearchHint for &[LinkPoint] {
-    /// # Arguments
-    /// - `offset`: linear displacement of front of train from initial starting
-    ///   position of back of train along entire PathTPC
-    /// - `idx`: index of front (TODO: clarify this) of train within corresponding [PathResCoeff]
-    /// - `dir`: direction of train along PathTPC
-    fn calc_idx(&self, offset: si::Length, mut idx: usize, dir: &Dir) -> anyhow::Result<usize> {
-        if dir != &Dir::Bwd {
-            ensure!(
-                offset <= self.last().unwrap().get_offset(),
-                "{}\nOffset in forward direction larger than last slice offset at idx: {}!",
-                format_dbg!(),
-                idx
-            );
-            while self[idx + 1].get_offset() < offset {
-                idx += 1;
-            }
-        } else if dir != &Dir::Fwd {
-            ensure!(
-                self.first().unwrap().get_offset() <= offset,
-                "{}\nOffset in reverse direction smaller than first slice offset at idx: {}!",
-                format_dbg!(),
-                idx
-            );
-            while offset < self[idx].get_offset() {
-                idx -= 1;
-            }
-        }
-        Ok(idx)
-    }
-}
-
 impl Valid for Vec<LinkPoint> {
     fn valid() -> Self {
         vec![

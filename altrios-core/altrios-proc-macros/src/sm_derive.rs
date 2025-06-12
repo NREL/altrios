d4a1b74136150b85f69f790201aef4e4eaee1792
Chad Baker <chad.baker@nrel.gov>
2025-06-12 12:11:56 -0600

    added some missing context

--- altrios-core/altrios-proc-macros/src/sm_derive.rs
+++ altrios-core/altrios-proc-macros/src/sm_derive.rs
@@ -122,8 +122,8 @@ pub(crate) fn state_methods_derive(input: TokenStream) -> TokenStream {
                 /// Implementation for structs with `save_interval`
                 fn save_state<F: Fn() -> String>(&mut self, loc: F) -> anyhow::Result<()> {
                     if let Some(interval) = self.save_interval {
-                        if *self.state.i.get_fresh(|| format!("{}\n{}.i has not been updated", format_dbg!(), stringify!(#ident)))? % interval == (0 as usize)
-                            || *self.state.i.get_fresh(|| format!("{}\n{}.i has not been updated", format_dbg!(), stringify!(#ident)))? == (1 as usize)
+                        if *self.state.i.get_fresh(|| format!("{}\n`{}.state.i` has not been updated", format_dbg!(), stringify!(#ident)))? % interval == (0 as usize)
+                            || *self.state.i.get_fresh(|| format!("{}\n`{}.state.i` has not been updated", format_dbg!(), stringify!(#ident)))? == (1 as usize)
                         {
                             #self_save_state
                             #(self.#fields_with_state.save_state(
